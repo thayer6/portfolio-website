c4ac1ce86dd1b0ebbdb6978b1de48b5ec469d7e2
thayer6 <casey.thayer6@gmail.com>
2023-11-16 14:08:04 -0800

    fix: disable twitter feed

--- src/portfolio.js
+++ src/portfolio.js
@@ -454,7 +454,7 @@ const contactInfo = {
 
 const twitterDetails = {
   userName: "casey_thayer_", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false// Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
